67fff9cb91ded0e715411a17f8b372c2ec61d5d2
Andrew McOlash <amcolash@gmail.com>
2015-05-09 22:41:24 -0500

    Basic ajax working

--- src/app.js
+++ src/app.js
@@ -1,58 +1,63 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
-var Vector2 = require('vector2');
+var ajax = require('ajax');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+var url = 'http://jsonplaceholder.typicode.com/posts/';
+
+var main = new UI.Menu({
+  sections: [{
+    items: [{
+      title: 'Pebble.js',
+      icon: 'images/menu_icon.png',
+      subtitle: 'Can do Menus'
+    }, {
+      title: 'Second Item',
+      subtitle: 'Subtitle Text'
+    }]
+  }]
+});
+
+main.on('select', function(e) {
+  console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+  console.log('The item is titled "' + e.item.title + '"');
+  
+  var loading = new UI.Card({
+    title: 'Loading...',
+    subtitle: 'Please Wait'
+  }).show();
+  
+  ajax(
+    {
+      url: url + (e.itemIndex + 1),
+      type: 'json'
+    },
+    
+    function(data) {
+      // Success!
+      console.log('Successfully fetched weather data!');
+      
+      loading.hide();
+      
+      new UI.Card({
+        title: data.id + ': ' + data.title,
+        subtitle: data.body,
+        scrollable: true
+      }).show();
+    },
+    
+    function(error) {
+      // Failure!
+      console.log('Failed fetching weather data: ' + error);
+      
+      loading.hide();
+      
+      new UI.Card({
+        title: 'Error!',
+        subtitle: error,
+        scrollable: true
+      }).show();
+    }
+  );
+  
 });
 
 main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
