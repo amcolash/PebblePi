fee1fe648d2088244026265e396bdf134f3b71c8
Andrew McOlash <amcolash@gmail.com>
2015-05-09 23:48:40 -0500

    Updated to get current temp

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 var express = require('express');
+
 var app = express();
 
+app.get('/', function(req, res) {
+  res.send({
+    title: 'Hi There!',
+    body: 'Hello from my Raspberry Pi!'
+  });
+});
+
 app.get('/temp', function(req, res) {
-  res.send('hello world');
+  var temp = '';
+
+  var exec = require('child_process').exec;
+  exec('/opt/vc/bin/vcgencmd measure_temp', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    temp = stdout.substring(5);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+      temp = 'Error getting current temp!';
+    }
+
+  res.json({
+    title: 'Current Temperature',
+    body: temp
+  });
+
+  });
+
 });
 
 var server = app.listen(3000, function () {
