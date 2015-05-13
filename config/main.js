957f2ce112e5cd7b713904980dd51c3627a6274a
Andrew McOlash <amcolash@gmail.com>
2015-05-13 16:16:23 -0500

    I think that the config is now working

--- config/main.js
+++ config/main.js
@@ -12,11 +12,26 @@ function saveOptions() {
   return options;
 }
 
+function getQueryParam(variable, defaultValue) {
+  // Find all URL parameters
+  var query = location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+
+    // If the query variable parameter is found, decode it to use and return it for use
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return defaultValue || false;
+}
+
 $().ready(function () {
   $("#b-cancel").click(function () {
     console.log("Cancel");
     var return_to = getQueryParam('return_to', 'pebblejs://close#');
-    document.location = return_to;
+    document.location = return_to + uncodeURIComponent('{}');
   });
 
   $("#b-submit").click(function () {
