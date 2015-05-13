c7fcf4a1b318a764690c09999e677186fbbdfdcb
Andrew McOlash <amcolash@gmail.com>
2015-05-13 13:38:57 -0500

    Added log timestamps, other small changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+require("console-stamp")(console, "mm/dd HH:MM:ss");
 
 var app = express();
 
@@ -14,9 +15,9 @@ app.get('/temp', function(req, res) {
 
   var exec = require('child_process').exec;
   exec('/opt/vc/bin/vcgencmd measure_temp', function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    temp = stdout.substring(5);
+    console.log('stdout: ' + stdout.replace(/(\r\n|\n|\r)/gm,''));
+    console.log('stderr: ' + stderr.replace(/(\r\n|\n|\r)/gm,''));
+    temp = stdout.substring(5).replace("'", ' °').replace('\n', '');
     if (error !== null) {
       console.log('exec error: ' + error);
       temp = 'Error getting current temp!';
@@ -34,5 +35,5 @@ app.get('/temp', function(req, res) {
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 });
